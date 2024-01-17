994019495ffc4b44012006078bdbf6684854c1f6
RunkleTyler <98990536+RunkleTyler@users.noreply.github.com>
2024-01-16 19:36:47 -0500

    16.2 Left Click Audio Added
    Implemented a sound when Left Clicking with the mouse.

--- Assets/Scripts/MouseClickEffect.cs
+++ Assets/Scripts/MouseClickEffect.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Neither GG nor AZ tutorial worked perfectly, had to cobble stuff together from both. 
+// Neither GG nor AZ tutorial worked perfectly for Particles on Left Click, had to cobble stuff together from both. 
 // Guillermo Gonzalez tutorial: https://www.youtube.com/watch?v=PI04VdO-j_0
 // Alexander Zotov tutorial: https://www.youtube.com/watch?v=xO_-qMJOmJc
+
+// RV used for sound when clicking. I changed from button to just Left Click. 
+// Rafael Vicuna tutorial: https://www.youtube.com/watch?v=HtiWt0SWxk8
 
 public class MouseClickEffect : MonoBehaviour
 {
     [SerializeField] private ParticleSystem testParticleSystem = default;   // GG
     private Vector2 mousePos;   // AZ
+    public AudioSource soundPlayer; // RV
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +29,7 @@ public class MouseClickEffect : MonoBehaviour
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // AZ
             testParticleSystem.Play();  // GG
             testParticleSystem.transform.position = new Vector3(mousePos.x, mousePos.y, 0f);    // AZ
+            soundPlayer.Play(); // RV
         }
     }
 }
